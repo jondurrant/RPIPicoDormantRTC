2a2bcb5571f62e780d13717da63445275f3ef5e3
Jon Durrant <jon@jondurrant.com>
2024-07-17 10:42:33 +0100

    Deep Sleep examples working but only if buils is in Debug mode

--- src/DeepSleep.cpp
+++ src/DeepSleep.cpp
@@ -10,12 +10,12 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 #include "hardware/rtc.h"
-#include "hardware/clocks.h"
 #include "hardware/rosc.h"
 #include "hardware/structs/scb.h"
 #include "hardware/sync.h"
 #include "hardware/rtc.h"
 #include "pico/util/datetime.h"
+
 
 DeepSleep::DeepSleep() {
 	 storeClocks();
@@ -35,22 +35,41 @@ void DeepSleep::gpio_callback(uint gpio, uint32_t events) {
 }
 
 void DeepSleep::sleep(uint8_t wakePad){
-	gpio_init(wakePad);
-	gpio_pull_up(wakePad);
-	gpio_set_dir(wakePad, GPIO_IN);
-	gpio_set_irq_enabled_with_callback (
-			wakePad,
-		   GPIO_IRQ_EDGE_FALL |
-		   GPIO_IRQ_EDGE_RISE,
-		   true,
-		   DeepSleep::gpio_callback
-		   );
+	if (wakePad <= 28){
+		gpio_init(wakePad);
+		gpio_pull_up(wakePad);
+		gpio_set_dir(wakePad, GPIO_IN);
+		uint core = get_core_num();
+		if (!xOwnGPIOCallbacks) {
+			//Have callback function so just enable
+			gpio_set_irq_enabled(
+						wakePad,
+					   GPIO_IRQ_EDGE_FALL |
+					   GPIO_IRQ_EDGE_RISE,
+					   true);
+		} else {
+			gpio_set_irq_enabled_with_callback(
+					wakePad,
+				   GPIO_IRQ_EDGE_FALL |
+				   GPIO_IRQ_EDGE_RISE,
+				   true,
+				   DeepSleep::gpio_callback
+				   );
+		}
+	}
 
 	sleep_run_from_xosc();
 	sleep_until_interupt();
 	recover_from_sleep(scb_orig, clock0_orig, clock1_orig);
 
-	gpio_disable_pulls(wakePad);
+	if (wakePad <= 28){
+		gpio_set_irq_enabled(
+				wakePad,
+			   GPIO_IRQ_EDGE_FALL |
+			   GPIO_IRQ_EDGE_RISE,
+			   false);
+		gpio_disable_pulls(wakePad);
+	}
 }
 
 void DeepSleep::rtcCB(void) {
@@ -75,21 +94,39 @@ void DeepSleep::sleep(uint minutes, uint8_t wakePad){
 			            .min   = 45,
 			            .sec   = 00
 			    };
-			 rtc_set_datetime(&t);
+			 if(!rtc_set_datetime(&t)){
+				 printf("RTC Set Failed\n");
+				 uart_default_tx_wait_blocking();
+			 }
+			 //Wait for RTC to update
+			 sleep_ms(250);
 		}
 		datetime_t t;
 		if (!rtc_get_datetime (&t)){
 			printf("RTC Broken\n");
 			uart_default_tx_wait_blocking();
 		}
+		//printf("Current Time %d:%d:%d \n", t.hour, t.min, t.sec);
+
 		t.year = -1;
 		t.month = -1;
 		t.day = -1;
 		t.hour = -1;
 		t.dotw = -1;
 		t.min = (t.min + minutes) %60;
-		t.min = -1;
-		t.sec = (t.sec + 10) % 60;
+		/*Debug
+		//t.min = -1;
+		//t.sec = (t.sec + 10) % 60;
+		printf("Sleep %u min until %d-%d-%d (%d) %d:%d:%d \n", minutes,
+				t.year,
+				t.month,
+				t.day,
+				t.dotw,
+				t.hour,
+				t.min,
+				t.sec);
+	    uart_default_tx_wait_blocking();
+	    */
 		rtc_set_alarm ( &t,  DeepSleep::rtcCB);
 	}
 	sleep(wakePad);
@@ -97,6 +134,10 @@ void DeepSleep::sleep(uint minutes, uint8_t wakePad){
 		pRTC->clear_alarm();
 	}
 	notifyObservers(minutes, true);
+}
+
+void DeepSleep::sleepMin(uint minutes){
+	sleep(minutes, 0xFF);
 }
 
 void DeepSleep::recover_from_sleep(uint scb_orig, uint clock0_orig, uint clock1_orig){
@@ -120,11 +161,11 @@ void DeepSleep::sleep_until_interupt( ) {
     // Turn off all clocks when in sleep mode except for RTC
 	if (pRTC == NULL){
 		clocks_hw->sleep_en0 =
+				xClocks |
 				CLOCKS_SLEEP_EN0_CLK_RTC_RTC_BITS
 				| CLOCKS_SLEEP_EN0_CLK_SYS_PWM_BITS;
 	} else {
-		clocks_hw->sleep_en0 =
-				CLOCKS_SLEEP_EN0_CLK_SYS_PWM_BITS;
+		clocks_hw->sleep_en0 = xClocks;
 	}
     clocks_hw->sleep_en1 = 0x0;
 
@@ -171,3 +212,51 @@ void DeepSleep::storeClocks(){
 	  clock0_orig = clocks_hw->sleep_en0;
 	  clock1_orig = clocks_hw->sleep_en1;
 }
+
+
+void DeepSleep::enablePWM(){
+	xClocks = xClocks | CLOCKS_SLEEP_EN0_CLK_SYS_PWM_BITS;
+}
+
+void DeepSleep::enableRTC(){
+	xClocks = xClocks |  CLOCKS_SLEEP_EN0_CLK_RTC_RTC_BITS;
+}
+
+void DeepSleep::enableJTAG(){
+	xClocks = xClocks |  CLOCKS_SLEEP_EN0_CLK_SYS_JTAG_BITS;
+}
+
+void DeepSleep::enableUart0(){
+	xClocks = xClocks |  CLOCKS_SLEEP_EN1_CLK_SYS_UART0_BITS;
+}
+
+
+void DeepSleep::enableUart1(){
+	xClocks = xClocks |  CLOCKS_SLEEP_EN1_CLK_PERI_UART0_BITS;
+}
+
+void DeepSleep::enableTimer(){
+	xClocks = xClocks |  CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS;
+}
+
+void DeepSleep::enableUSB(){
+	xClocks = xClocks |  CLOCKS_ENABLED0_CLK_SYS_PLL_USB_BITS;
+}
+
+void DeepSleep::enablePIO0(){
+	xClocks = xClocks |  CLOCKS_ENABLED0_CLK_SYS_PADS_BITS;
+}
+
+void DeepSleep::enablePIO1(){
+	xClocks = xClocks |  CLOCKS_ENABLED0_CLK_SYS_PIO1_BITS;
+}
+
+
+void DeepSleep::enableDMA(){
+	xClocks = xClocks |  CLOCKS_ENABLED0_CLK_SYS_DMA_BITS;
+}
+
+void DeepSleep::setOwnGPIOCallbacks(bool on){
+	xOwnGPIOCallbacks = on;
+}
+
--- src/DeepSleep.h
+++ src/DeepSleep.h
@@ -12,6 +12,7 @@
 #include "DS3231.hpp"
 #include "DormantNotification.h"
 #include <list>
+#include "hardware/clocks.h"
 
 class DeepSleep {
 public:
@@ -24,19 +25,35 @@ public:
 	 */
 	void setRTC(DS3231 *rtc);
 
+
+	/***
+	 * Own GPIO Callbacks, meaning any existing GPIO callback on the
+	 * core will no longer function.
+	 * If using local RTC to wake core this can be disabled
+	 * @param on
+	 */
+	void setOwnGPIOCallbacks(bool on=true);
+
 	/***
 	 * Sleep until pad pulled to ground
-	 * @param wakePad - GPIO Pad for wake
+	 * @param wakePad - GPIO Pad for wake. >28 GPIO wake is not enabled
 	 */
-	void sleep(uint8_t wakePad);
+	void sleep(uint8_t wakePad = 0xFF);
 
 	/***
 	 * Sleep for number of minutes and wake by GPIO pad
 	 * If no RTC then it will just do sleep(wakePad)
-	 * @param minutes - Minutes to sleep for
-	 * @param wakePad - GPIO Pad for wake
+	 * @param minutes - Minutes to sleep for (<=60)
+	 * @param wakePad - GPIO Pad for wake. >28 GPIO wake is not enabled
 	 */
-	void sleep(uint minutes, uint8_t wakePad);
+	void sleep(uint minutes, uint8_t wakePad=0xFF);
+
+	/***
+	 * Sleep for a number of minutes.
+	 * Assume woken by internal RTC
+	 * @param minutes <=60
+	 */
+	void sleepMin(uint minutes);
 
 
 	/***
@@ -45,8 +62,76 @@ public:
 	 */
 	static DeepSleep * singleton();
 
+	/***
+	 * Add observer for sleep and wakeup
+	 * @param obs
+	 */
 	void addObserver(DormantNotification *obs);
+
+	/***
+	 * Delete observer for sleep and wakeup
+	 * @param obs
+	 */
 	void delObserver(DormantNotification *obs);
+
+
+	/***
+	 * Enable PWM to function during Deep Sleep
+	 */
+	void enablePWM();
+
+	/***
+	 * Enable Pico RTC to function during Deep Sleep
+	 */
+	void enableRTC();
+
+	/***
+	 * Untested. Enable JTAG and therefore SWD during deepsleep?
+	 */
+	void enableJTAG();
+
+	/***
+	 * UnTested
+	 * Enable Uart0 receive  during deep sleep
+	 */
+	void enableUart0();
+
+	/***
+	 * UnTested
+	 * Enable Uart1 receive  during deep sleep
+	 */
+	void enableUart1();
+
+	/***
+	 * UnTested
+	 * Enable Timers  during deep sleep
+	 */
+	void enableTimer();
+
+	/***
+	 * UnTested
+	 * Enable USB receive  during deep sleep
+	 */
+	void enableUSB();
+
+	/***
+	 * UnTested
+	 * Enable PIO0   during deep sleep
+	 */
+	void enablePIO0();
+
+	/***
+	 * UnTested
+	 * Enable PIO1   during deep sleep
+	 */
+	void enablePIO1();
+
+	/***
+	 * UnTested
+	 * Enable DMA   during deep sleep
+	 */
+	void enableDMA();
+
 
 private:
 	static DeepSleep * pSingleton ;
@@ -82,10 +167,12 @@ private:
 
 	std::list<DormantNotification *> xObservers;
 
+	bool xOwnGPIOCallbacks = true;
 	DS3231 *pRTC = NULL;
 	 uint scb_orig;
 	 uint clock0_orig;
 	 uint clock1_orig;
+	 io_rw_32 xClocks = 0;
 };
 
 #endif /* SRC_DEEPSLEEP_H_ */
